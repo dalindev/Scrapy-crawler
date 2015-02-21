fafe817ad7affe247035fad3e9bb90fa14ceab12
JavaScript rule them ALL <hdltyty@163.com>
2015-02-20 19:16:58 -0500

    scrapy01

---
+++ ScrapyDalinHuang/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ScrapyDalinHuang project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ScrapyDalinHuang'
+
+SPIDER_MODULES = ['ScrapyDalinHuang.spiders']
+NEWSPIDER_MODULE = 'ScrapyDalinHuang.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ScrapyDalinHuang (+http://www.yourdomain.com)'
